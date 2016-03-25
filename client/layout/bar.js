bbe449efca2826172e8d93edc6517bbc45e456ec
slengtengriddim <raggamuffin@openmailbox.org>
2016-03-25 12:05:55 +0100

    updated routing for entering url manually

--- client/layout/bar.js
+++ client/layout/bar.js
@@ -1,13 +1,15 @@
-var ATTENTION_MODE = 'attentionMode';
+ATTENTION_MODE = 'attentionMode';
 Session.setDefault(ATTENTION_MODE, false);
 
+LAST_PATH = 'lastPath';
+Session.setDefault(LAST_PATH, '/');
 
 Template.bar.events({
-	'click .attention-mode': function() {
-		var oldValue = Session.get('attentionMode') || false;
-		Session.set('attentionMode', !oldValue);
+  'click .attention-mode': function() {
+    var oldValue = Session.get(ATTENTION_MODE) || false;
+    Session.set(ATTENTION_MODE, !oldValue);
 
-		var routePath = FlowRouter.current().route.path;
-		Session.set('lastPath', routePath);
-	}
+    var routePath = FlowRouter.current().path;
+    Session.set(LAST_PATH, routePath);
+  }
 });
